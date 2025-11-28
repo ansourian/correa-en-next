9e54d317b4afbcfda9fa512f9a8971db0b99ea92
ansourian <jairansourian@gmail.com>
2025-11-28 13:19:10 -0300

    nueva notificacion de zoom en pantalla modelos

--- src/components/GaleriaModelos.jsx
+++ src/components/GaleriaModelos.jsx
@@ -1,33 +1,35 @@
-"use client";
+"use client"
 
-import React, { useState, useRef, useEffect } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import { Modal, Button } from "react-bootstrap"; // Asegúrate de tener react-bootstrap instalado
+import React, { useState, useRef, useEffect } from "react"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import { Modal, Button } from "react-bootstrap" // Asegúrate de tener react-bootstrap instalado
+import { AdsClick } from "@mui/icons-material"
 
 export default function GaleriaModelos({ images }) {
-  const [selectedImage, setSelectedImage] = useState(images[0]);
-  const [modalOpen, setModalOpen] = useState(false); // Estado para controlar la apertura del modal
-  const [activeImageIndex, setActiveImageIndex] = useState(0); // Estado para almacenar el índice de la imagen seleccionada
-  const [isZoomed, setIsZoomed] = useState(false); // Estado para controlar el zoom de la imagen
-  const [isMobile, setIsMobile] = useState(false); // Estado para detectar si es móvil
+  const [selectedImage, setSelectedImage] = useState(images[0])
+  const [modalOpen, setModalOpen] = useState(false) // Estado para controlar la apertura del modal
+  const [activeImageIndex, setActiveImageIndex] = useState(0) // Estado para almacenar el índice de la imagen seleccionada
+  const [isZoomed, setIsZoomed] = useState(false) // Estado para controlar el zoom de la imagen
+  const [isMobile, setIsMobile] = useState(false) // Estado para detectar si es móvil
+  const [showHint, setShowHint] = useState(true)
 
-  const sliderRef = useRef(null); // Crear una referencia para el slider
+  const sliderRef = useRef(null) // Crear una referencia para el slider
 
   // Detectar si el dispositivo es móvil o no
   useEffect(() => {
     const checkIfMobile = () => {
-      setIsMobile(window.innerWidth <= 768); // 768px es el límite para considerar un dispositivo móvil
-    };
+      setIsMobile(window.innerWidth <= 768) // 768px es el límite para considerar un dispositivo móvil
+    }
 
-    checkIfMobile();
-    window.addEventListener("resize", checkIfMobile);
+    checkIfMobile()
+    window.addEventListener("resize", checkIfMobile)
 
     return () => {
-      window.removeEventListener("resize", checkIfMobile);
-    };
-  }, []);
+      window.removeEventListener("resize", checkIfMobile)
+    }
+  }, [])
 
   // Configuración del slider para la galería principal (imagen grande)
   const settings = {
@@ -38,54 +40,79 @@ export default function GaleriaModelos({ images }) {
     slidesToScroll: 1,
     adaptiveHeight: true,
     beforeChange: (current, next) => {
-      setSelectedImage(images[next]); // Actualiza la imagen seleccionada cuando el slider cambia
+      setSelectedImage(images[next]) // Actualiza la imagen seleccionada cuando el slider cambia
+      setShowHint(false)
     },
-  };
+  }
 
   // Función para manejar el clic en la imagen y abrir el modal
   const handleImageClick = (index) => {
-    setActiveImageIndex(index); // Establece el índice de la imagen seleccionada
-    setModalOpen(true); // Abre el modal
-    setIsZoomed(false); // Al abrir el modal, inicializamos el estado del zoom en falso
-  };
+    setActiveImageIndex(index) // Establece el índice de la imagen seleccionada
+    setModalOpen(true) // Abre el modal
+    setIsZoomed(false) // Al abrir el modal, inicializamos el estado del zoom en falso
+  }
 
   // Función para manejar el clic en una miniatura
   const handleThumbnailClick = (image, index) => {
-    setSelectedImage(image); // Cambia la imagen seleccionada al hacer clic en una miniatura
+    setSelectedImage(image) // Cambia la imagen seleccionada al hacer clic en una miniatura
     if (sliderRef.current) {
-      sliderRef.current.slickGoTo(index); // Desplaza el slider a la miniatura seleccionada
+      sliderRef.current.slickGoTo(index) // Desplaza el slider a la miniatura seleccionada
     }
-  };
+  }
 
   // Función para alternar el zoom en la imagen
   const handleZoomToggle = () => {
-    if (isMobile) return;
-    setIsZoomed(!isZoomed); // Cambia el estado de zoom cada vez que se hace clic
-  };
+    if (isMobile) return
+    setIsZoomed(!isZoomed) // Cambia el estado de zoom cada vez que se hace clic
+  }
+
+  const DEBUG_HINT = true
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowHint(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     <div>
       {/* Galería deslizable de imágenes grandes */}
       <div className="main-gallery">
-        <Slider {...settings} ref={sliderRef}>
-          {images.map((img, index) => (
-            <div key={index}>
-              <img
-                src={img}
-                alt={`Imagen grande ${index + 1}`}
-                style={{
-                  width: "100%",
-                  maxHeight: "650px",
-                  objectFit: "contain",
-                  cursor: "zoom-in", // Indica que se puede hacer zoom
-                  outline: "none", // Evita el outline al hacer clic
-                  border: "none", // Asegura que no haya borde
-                }}
-                onClick={() => handleImageClick(index)} // Al hacer clic en la imagen, abre el modal
-              />
-            </div>
-          ))}
-        </Slider>
+        <div
+          onClick={() => setShowHint(false)}
+          className={`div-click-zoom ${showHint ? "visible" : "hidden"}`}
+        >
+          <AdsClick />
+          <p className="p-div-click-zoom">Click en la imágen para hacer zoom</p>
+        </div>
+        <div
+          className="slider-wrapper"
+          onClick={() => setShowHint(false)} // click estándar
+          onMouseDown={() => setShowHint(false)} // arranque de drag en desktop
+          onTouchStart={() => setShowHint(false)} // arranque de swipe en mobile
+        >
+          <Slider {...settings} ref={sliderRef}>
+            {images.map((img, index) => (
+              <div key={index}>
+                <img
+                  src={img}
+                  alt={`Imagen grande ${index + 1}`}
+                  style={{
+                    width: "100%",
+                    maxHeight: "650px",
+                    objectFit: "contain",
+                    cursor: "zoom-in", // Indica que se puede hacer zoom
+                    outline: "none", // Evita el outline al hacer clic
+                    border: "none", // Asegura que no haya borde
+                  }}
+                  onClick={() => handleImageClick(index)} // Al hacer clic en la imagen, abre el modal
+                />
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
 
       {/* Miniaturas debajo de la galería */}
@@ -158,12 +185,10 @@ export default function GaleriaModelos({ images }) {
         {/* Botón para cerrar el modal */}
         <Modal.Footer>
           <div className="div-button" onClick={() => setModalOpen(false)}>
-            <a className="button-index">
-              CERRAR
-            </a>
+            <a className="button-index">CERRAR</a>
           </div>
         </Modal.Footer>
       </Modal>
     </div>
-  );
+  )
 }
